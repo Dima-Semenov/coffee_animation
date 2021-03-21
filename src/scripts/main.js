c9581d87e7c46753f7eb637019b14f5e1a938100
Dima Semenov <semenovdima998@gmail.com>
2021-03-21 17:44:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const milk = document.querySelector('.coffee__photo2');
+const coffee = document.querySelector('.coffee__photo');
+
+window.addEventListener('scroll', () => {
+  const width = +window.pageYOffset + 150 + 'px';
+  const height = +window.pageYOffset - 50 + 'px';
+
+  milk.style.width = width;
+  milk.style.height = height;
+
+  if (+window.pageYOffset > 750) {
+    milk.style.width = '900px';
+    milk.style.height = '640px';
+
+    coffee.style.position = 'absolute';
+    coffee.style.top = '850px';
+  } else {
+    milk.style.width = width;
+    milk.style.height = height;
+
+    coffee.style.position = 'fixed';
+    coffee.style.top = '50px';
+  }
+});
